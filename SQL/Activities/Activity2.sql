7c06c0c0d8bf1d78bca628fe3c5699044888f65b
Dhawal Bhatia <dhbhatia@in.ibm.com>
2021-06-29 22:18:04 +0530

    sql activities

---
+++ SQL/Activities/Activity2.sql
@@ -0,0 +1,11 @@
+REM   Script: Activity2
+
+INSERT ALL 
+    INTO salesman VALUES(5008, 'Tom Cruise', 'Paris', 22) 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'Mcanzie', 'Paris', 15) 
+    INTO salesman VALUES(5007, 'Eve Adam', 'Rome', 23) 
+    INTO salesman VALUES(5003, 'Lauren Ben', 'San Jose', 10) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,10 @@
+REM   Script: Activity5
+
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='Tom Cruise';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='Mcanzie';
+
+SELECT * FROM salesman;
