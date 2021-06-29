f3a8774622d3159b0489681b012bd4777ae19949
Dhawal Bhatia <dhbhatia@in.ibm.com>
2021-06-29 23:51:08 +0530

    Python activities

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+name= input('Please enter your name -> ')
+age= int(input('Please enter your Age -> '))
+year = str((2020-age)+100)
+print (name + ' Will be of 100years old in the year ->' + year)
---
+++ Python/Activities/Activity7.py
@@ -0,0 +1,5 @@
+total = 0
+list1 = [10, 9, 5, 12, 10]
+for ele in range(0, len(list1)):
+    total = total + list1[ele]
+print("Sum of all elements in given list> ", total)
